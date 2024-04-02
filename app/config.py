7d93c8d0520bf95614cd06bdce5be29951c60c2a
Mcrâck <ulisesalejandro98@hotmail.com>
2024-04-02 13:18:59 -0600

    a

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ DB_USER=os.environ.get("DB_USER")
 DB_PASS=os.environ.get("DB_PASS")
 DB_HOST=os.environ.get("DB_HOST")
 DB_NAME=os.environ.get("DB_NAME")
-DB_SSL=os.environ.get("DB_SSL")
+DB_SSL=os.environ.get("NIX_SSL_CERT_FILE")
 SECRET_K=os.environ.get("SECRET_K")
 
 
@@ -17,9 +17,9 @@ path=os.getcwd()
 
 
 
+path2=f"{path}/{DB_SSL}"
 
-
-print(os.environ)
+print(DB_SSL)
 
 
 
@@ -27,7 +27,7 @@ print(os.environ)
 
 class Config:
     SECRET_KEY=f"{SECRET_K}"
-    SQLALCHEMY_DATABASE_URI=f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?ssl_ca={path2}"
+    SQLALCHEMY_DATABASE_URI=f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?ssl_ca={DB_SSL}"
 
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     
